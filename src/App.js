0406281bb905b4d38b23dc255935ab6f39e138f2
BharathRajKumarBiswal <bharathraj675@pec.edu>
2024-09-14 14:02:02 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import './App.css';
 import Todolist from './Component/Todolist';
 import Tododisplay from "./Component/Tododisplay";
-// import UpdateTodolist from "./Component/UpdateTodolist";
-// import UpdateTododisplay from "./Component/UpdateTododisplay";
+
 
 function App() {
   return (
@@ -13,8 +12,7 @@ function App() {
         <Route path="/" element={<Todolist/>} />
         <Route path="/Todolost" element={<Todolist/>}/>
         <Route path="/Tododisplay" element={<Tododisplay/>}/>
-        {/* <Route path="/UpdateTodolist" element={<UpdateTodolist/>}/>
-        <Route path="/UpdateTododisplay" element={<UpdateTododisplay/>}/> */}
+        
         
       </Routes>
      </Router>
